afb26ef81b69ed39768326ac89326755ee993dfd
Alphanym <rectangletangle@gmail.com>
2018-06-24 11:37:36 -0700

    fix failing setup script

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 
 import setuptools
 
-with open('README.md') as f:
-    setuptools.setup(
-        name='alphanym',
-        version='1.0.0',
-        description='Official Alphanym Python Client',
-        long_description=f.read().strip(),
-        author='Drew French',
-        author_email='french@alphanym.com',
-        url='https://www.alphanym.com',
-        py_modules=[
-            'alphanym',
-        ],
-        install_requires=[
-            'requests',
-        ],
-        license='MIT License',
-        zip_safe=False,
-        keywords='Alphanym',
-        classifiers=[],
-    )
+setuptools.setup(
+    name='alphanym',
+    version='1.0.1',
+    description='Official Alphanym Python Client',
+    long_description='Official Alphanym Python Client',
+    author='Drew French',
+    author_email='french@alphanym.com',
+    url='https://www.alphanym.com',
+    py_modules=[
+        'alphanym',
+    ],
+    install_requires=[
+        'requests',
+    ],
+    license='MIT License',
+    zip_safe=False,
+    keywords='Alphanym',
+    classifiers=[],
+)
